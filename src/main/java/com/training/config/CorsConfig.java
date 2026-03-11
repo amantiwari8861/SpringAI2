b8cb3d83d51c2f795ff88119a4dc4163cd5605bb
Aman Tiwari <amantiwari8861@gmail.com>
2026-03-11 17:30:42 +0530

    cors fixed

--- src/main/java/com/training/config/CorsConfig.java
+++ src/main/java/com/training/config/CorsConfig.java
@@ -3,37 +3,22 @@ package com.training.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-
         CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowedOriginPatterns(List.of(
-                "https://*.iamandroid.in",
-                "http://localhost:*"
-        ));
-
-        config.setAllowedMethods(List.of(
-                "GET","POST","PUT","DELETE","OPTIONS"
-        ));
-
+        config.setAllowedOrigins(List.of("http://localhost:5173", "http://iamandroid.in", "https://iamandroid.in", "https://www.iamandroid.in", "http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-
         config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return source;
     }
 }
